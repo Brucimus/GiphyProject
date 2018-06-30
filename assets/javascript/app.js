6acd1ddfd59a75c3c81a0f8121230ae60873fc20
Brucimus <Brucimus@gmail.com>
2018-06-30 12:24:57 -0700

    add layout in comment

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+//variables
+var animeList = ["dragonball", "gundam wing", "gundam seed", "pokemon", "digimon", "naruto", "full metal alchemist", "full metal panic"]
+
+//clear button listings function
+
+//clear displayed gifst function
+
+//loop through animeList array for display
+
+//on animeButton click
+$(".animeButton").on("click", function() {
+    //function specific variables
+    var buttonValue = $(this).attr("value");
+
+
+})
+
+//add new item to animeList on Click function
