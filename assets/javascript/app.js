ce33def72124db44e357029ecafae8c563cb9bac
Brucimus <Brucimus@gmail.com>
2018-06-30 21:33:49 -0700

    create list of buttons for each array item and create clear gif location

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,19 @@
 var animeList = ["dragonball", "gundam wing", "gundam seed", "pokemon", "digimon", "naruto", "full metal alchemist", "full metal panic"]
 
 //clear button listings function
+function displayButtons() {
+    for (var i = 0; i < animeList.length; i++) {
+        var buttonCreate = $("<button>" + animeList[i] + "</button>");
+        buttonCreate.attr("value", animeList[i]);
+        $("#listButtons").append(buttonCreate);
+    }
+}
 
+displayButtons();
 //clear displayed gifst function
+function clearGifs() {
+    $("#listGifs").empty();
+}
 
 //loop through animeList array for display
 
