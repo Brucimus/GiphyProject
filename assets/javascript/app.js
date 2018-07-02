2634241fc576857b343a0dc070ed8908dc32203a
Brucimus <Brucimus@gmail.com>
2018-07-01 23:19:41 -0700

    changed spacing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,64 +25,62 @@ function displayButtons() {
 
     //on animeButton click
     $(".animeButton").click(function() {
-    //function specific variables
-    var buttonValue = $(this).attr("value");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + buttonValue +"&api_key=KVdYfEzpM2XtyY8DMGDjdoamhv13jNZt&limit=10";
+        //function specific variables
+        var buttonValue = $(this).attr("value");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + buttonValue +"&api_key=KVdYfEzpM2XtyY8DMGDjdoamhv13jNZt&limit=10";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function(data) {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(data) {
 
-        //clear gifs from display
-        clearGifs();
-        var results = data.data;
+            //clear gifs from display
+            clearGifs();
+            var results = data.data;
 
-        //iterate through pulled gifs to display Ratings and Gifs
-        for (var i = 0; i < results.length ; i++) {
-            
-            //create div to house a gif's properties
-            var gifDiv = $("<div class='gifImages'>");
-            var gifRating = results[i].rating;
-            var ratingDisplay = $("<p>").text("Rating: " + gifRating);            
-            var animeImage = $("<img>");
+            //iterate through pulled gifs to display Ratings and Gifs
+            for (var i = 0; i < results.length ; i++) {
+                
+                //create div to house a gif's properties
+                var gifDiv = $("<div class='gifImages'>");
+                var gifRating = results[i].rating;
+                var ratingDisplay = $("<p>").text("Rating: " + gifRating);            
+                var animeImage = $("<img>");
 
-            //source still and gif images
-            animeImage.attr( {
-                "src" : results[i].images.fixed_height_still.url,
-                "active" : results[i].images.fixed_height.url,
-                "still" : results[i].images.fixed_height_still.url,
-                "currentStatus" : "gifStill",
-                "class" : 'gifImages'
-            });
+                //source still and gif images
+                animeImage.attr( {
+                    "src" : results[i].images.fixed_height_still.url,
+                    "active" : results[i].images.fixed_height.url,
+                    "still" : results[i].images.fixed_height_still.url,
+                    "currentStatus" : "gifStill",
+                    "class" : 'gifImages'
+                });
 
-            //append rating display
-            gifDiv.append(ratingDisplay);
+                //append rating display
+                gifDiv.append(ratingDisplay);
 
-            //append gif image
-            gifDiv.append(animeImage);
+                //append gif image
+                gifDiv.append(animeImage);
 
-            //display div of gif properties
-            $("#listGifs").append(gifDiv);
-        }
-
-        //play and stop gifs
-        $(".gifImages").click(function() {
-            var gifStatus = $(this).attr("currentStatus");
-
-            if (gifStatus === "gifStill") {
-                $(this).attr("src", $(this).attr("active"));
-                $(this).attr("currentStatus","gifActive");
-            } else if (gifStatus === "gifActive") {
-                $(this).attr("src", $(this).attr("still"));
-                $(this).attr("currentStatus","gifStill");
+                //display div of gif properties
+                $("#listGifs").append(gifDiv);
             }
+
+            //play and stop gifs
+            $(".gifImages").click(function() {
+                var gifStatus = $(this).attr("currentStatus");
+
+                if (gifStatus === "gifStill") {
+                    $(this).attr("src", $(this).attr("active"));
+                    $(this).attr("currentStatus","gifActive");
+                } else if (gifStatus === "gifActive") {
+                    $(this).attr("src", $(this).attr("still"));
+                    $(this).attr("currentStatus","gifStill");
+                }
+            });
         });
     });
-});
-
-
 }
 
 displayButtons();
